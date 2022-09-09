004ef1c2f06194a960921932208a91a55f7fdd63
Francesco <francesco.cubito@gmail.com>
2022-09-09 16:29:36 +0000

    Temporary save 3

--- machinetranslation/translator.py
+++ machinetranslation/translator.py
@@ -16,7 +16,7 @@ url = os.environ["url"]
 
 authenticator = IAMAuthenticator(f"{apikey}")
 language_translator = LanguageTranslatorV3(
-    version="1.0",
+    version="2022-09-09",
     authenticator=authenticator
 )
 
@@ -26,16 +26,18 @@ def english_to_french(englishText):
     """
     Translate english text to french text
     """
-    frenchText = language_translator.translate(englishText, "en-fr").get_result()
+    frenchText = language_translator.translate(text=englishText, 
+                                               model_id="en-fr").get_result()
     
-    return json.dumps(frenchText)["translations"][0]["translation"]
+    return frenchText["translations"][0]["translation"]
 
 def french_to_english(frenchText):
     """
     Translate french text to english text
     """
-    englishText = language_translator.translate(englishText, "fr-en").get_result()
+    englishText = language_translator.translate(text=frenchText, 
+                                                model_id="fr-en").get_result()
     
-    return json.dumps(englishText)["translations"][0]["translation"]
+    return englishText["translations"][0]["translation"]
 
 print(english_to_french("Hello to everyone"))
