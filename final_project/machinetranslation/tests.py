d205206db7a17c0ad015a791f3dfbee7e1c582f8
francesco-cubito97 <francesco.cubito@gmail.com>
2022-09-12 09:14:16 +0000

    Final Commit

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -3,11 +3,11 @@ from translator import english_to_french, french_to_english
 
 class TestTranslator(unittest.TestCase):
     def test_e2f(self):
-        self.assertEqual(english_to_french(" ").lower(), " ".lower())
+        self.assertNotEqual(english_to_french("Hello").lower(), " ".lower())
         self.assertEqual(english_to_french("Hello").lower(), "Bonjour".lower())
 
     def test_f2e(self):
-        self.assertEqual(french_to_english(" ").lower(), " ".lower())
+        self.assertNotEqual(french_to_english("Bonjour").lower(), " ".lower())
         self.assertEqual(french_to_english("Bonjour").lower(), "Hello".lower())
 
 if __name__ == '__main__':
